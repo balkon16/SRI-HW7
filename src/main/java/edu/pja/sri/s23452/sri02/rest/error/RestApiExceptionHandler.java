fb2bf3bd0497e76ea29970512f4d8b6536c1925d
balkon16 <pawel.k.lonca@gmail.com>
2021-06-27 16:52:33 +0200

    work in progress

--- src/main/java/edu/pja/sri/s23452/sri02/rest/error/RestApiExceptionHandler.java
+++ src/main/java/edu/pja/sri/s23452/sri02/rest/error/RestApiExceptionHandler.java
@@ -42,11 +42,8 @@ public class RestApiExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    private ResponseEntity<ErrorMessage> handleEntityNotFound(EntityNotFoundException ex){
-        // TODO: trzeba dodać exception handler -> ktoś się zapytał na Teams:
-        //  https://teams.microsoft.com/l/message/19:e2e3801ed850422cba468d23ccac6bfb@thread.tacv2/1624542934649?tenantId=ae65f568-0ceb-42c2-9dda-731b9c16e6b4&groupId=e0353399-d526-4c3d-9024-0b10a46b3791&parentMessageId=1624363820474&teamName=2021L_win_SRI_1w&channelName=General&createdTime=1624542934649
-        //  poniższe działa
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    private ResponseEntity<ErrorMessage> handleEntityNotFound(EntityNotFoundException ex) {
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
